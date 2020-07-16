37e2ec202c5c870c28286dcda91d54838b780dce
woojiahao <woojiahao1234@gmail.com>
2020-07-16 17:21:08 +0800

    Add Quote component for blockquotes

---
+++ src/components/Quote.vue
@@ -0,0 +1,48 @@
+<template>
+  <blockquote>
+    <h3>{{title}}</h3>
+    <slot></slot>
+  </blockquote>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator"
+
+  @Component
+  export default class Quote extends Vue {
+    @Prop() readonly title: string | undefined
+  }
+</script>
+
+<style scoped>
+  blockquote {
+    position: relative;
+    display: block;
+    color: var(--gray);
+    margin-bottom: 2em;
+    padding: 1em 0;
+  }
+
+  blockquote h3 {
+    text-align: start;
+    font-family: var(--sans-serif-font);
+    font-size: 1.25em;
+    padding: 0 0 1em 0;
+    color: #fafafa;
+  }
+
+  blockquote p:last-child {
+    padding: 0;
+    margin: 0;
+  }
+
+  blockquote::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    bottom: 0;
+    left: -5%;
+    width: 10px;
+    background-color: var(--blue);
+  }
+</style>
